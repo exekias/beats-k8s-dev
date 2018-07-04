47520556d0a778d0156d616f37f5981e6410988b
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2018-07-04 16:00:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# beats-k8s-dev
+Easily bootstrap Kubernetes with Elasticsearch, Kibana and Beats for development
---
+++ beats-kube-up.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+
+K8S_VERSION="v1.10.0"
+MINIKUBE_VERSION="v0.28.0"
+#TODO set beats version here
+#BEATS_VERSION="6.3.0"
+MEMORY_MB="8192"
+BIN=./bin
+
+case $(uname -s) in
+   Darwin) OS=darwin ;;
+   Linux) OS=linux ;;
+   *)
+     echo "This script only works on Linux and Mac"
+     exit 1
+     ;;
+esac
+
+case $(uname -m) in
+    i?86) ARCH=i386 ;;
+    x86_64) ARCH=amd64 ;;
+    *)
+      echo "Unknown arch"
+      exit 1
+      ;;
+esac
+
+
+mkdir -p $BIN
+PATH=$BIN:$PATH
+
+if [ ! -f $BIN/minikube ]; then
+  echo "Downloading minikube..."
+  curl -Lo $BIN/minikube https://storage.googleapis.com/minikube/releases/$MINIKUBE_VERSION/minikube-$OS-$ARCH
+  chmod +x $BIN/minikube
+  echo ""
+fi
+
+if [ ! -f $BIN/kubectl ]; then
+  echo "Downloading kubectl..."
+  curl -Lo $BIN/kubectl https://storage.googleapis.com/kubernetes-release/release/$K8S_VERSION/bin/$OS/$ARCH/kubectl
+  chmod +x $BIN/kubectl
+  echo ""
+fi
+
+$BIN/minikube start --kubernetes-version=$K8S_VERSION --memory $MEMORY_MB
+echo ""
+
+echo "Deploying services..."
+echo "kube-sate-metrics..."
+$BIN/kubectl create -f manifests/kube-state-metrics.yaml
+echo ""
+
+echo "elasticsearch..."
+$BIN/kubectl create -f manifests/elasticsearch.yaml
+echo ""
+
+echo "kibana..."
+$BIN/kubectl create -f manifests/kibana.yaml
+echo ""
+
+echo "Cluster is ready. Once deployments are done Kibana will be available under: http://$(minikube ip):5601"
+echo "To kill current deployments and stop the cluster just run:"
+echo "  $BIN/minikube delete"
